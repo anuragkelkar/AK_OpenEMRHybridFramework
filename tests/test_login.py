538793271c4ad7ea9c34ba17a07db652819ce823
Kelkar <AKELKAR1@jaguarlandrover.com>
2024-02-02 09:11:59 +0000

    pytest fixtures added

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,22 +1,23 @@
 from assertpy import assert_that
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pytest
 
 
 class TestLoginUi:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.maximize_window()
+        self.driver.get("http://demo.openemr.io/b/openemr/interface/login/login.php?site=default")
+        yield
+        self.driver.quit()
+
     def test_title(self):
-        driver = webdriver.Chrome()
-        driver.implicitly_wait(5)
-        driver.maximize_window()
-        driver.get("http://demo.openemr.io/b/openemr/interface/login/login.php?site=default")
-        actual_title = driver.title
+        actual_title = self.driver.title
         assert_that("OpenEMR Login").__eq__(actual_title)
 
     def test_app_description(self):
-        driver = webdriver.Chrome()
-        driver.implicitly_wait(5)
-        driver.maximize_window()
-        driver.get("http://demo.openemr.io/b/openemr/interface/login/login.php?site=default")
-        actual_desc = driver.find_element(By.XPATH, "//p[@class='text-center lead']").text
-        # assert actual_title.__eq__("OpenEMR Login")
+        actual_desc = self.driver.find_element(By.XPATH, "//p[@class='text-center lead']").text
         assert_that(actual_desc).contains("Electronic Health Record")
