4584d0348e45023ed6a219759c61ec51c8bc9960
Kelkar <AKELKAR1@jaguarlandrover.com>
2024-02-05 08:33:12 +0000

    added page classes

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,2 +1,19 @@
+from selenium.webdriver.common.by import By
+
+
 class LoginPage:
-    pass
+    def __init__(self, driver):
+        self.__browser = driver
+
+    def enter_username(self, username):
+        self.__browser.find_element(By.ID, "authUser").send_keys(username)
+
+    def enter_password(self, password):
+        self.__browser.find_element(By.ID, "clearPass").send_keys(password)
+
+    def click_login(self):
+        self.__browser.find_element(By.ID, "login-button").click()
+
+    def get_error_msg(self):
+        error_msg = self.__browser.find_element(By.XPATH, "//*[contains(text(),'Invalid')]").text
+        return error_msg
--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 from base.automation_wrapper import WebDriverWrapper
 from utils.data_utils import DataSource
+from pages.login_page import LoginPage
 
 
 # inherited class WebDriverWrapper class to reuse driver variable
@@ -12,24 +13,22 @@ class TestLoginFunction(WebDriverWrapper):
     @pytest.mark.parametrize("username, password, expected_title", DataSource.valid_testdata)
     def test_valid_login(self, username, password, expected_title):
         print("validation login")
-        self.driver.find_element(By.ID, "authUser").send_keys(username)
-        self.driver.find_element(By.ID, "clearPass").send_keys(password)
-        self.driver.find_element(By.ID, "login-button").click()
-        actual_title = self.driver.title
-        print(actual_title)
-        assert_that(actual_title).matches(expected_title)
+        login = LoginPage(self.driver)
+        login.enter_username(username)
+        login.enter_username(password)
+        login.click_login()
+        assert_that(self.driver.title).matches(expected_title)
 
     @pytest.mark.parametrize("username, password, expected_title", DataSource.invalid_testdata)
     def test_invalid_login(self, username, password, expected_title):
-        self.driver.find_element(By.ID, "authUser").send_keys(username)
-        self.driver.find_element(By.ID, "clearPass").send_keys(password)
-        self.driver.find_element(By.ID, "login-button").click()
-        error_msg = self.driver.find_element(By.XPATH, "//*[contains(text(),'Invalid')]").text
+        login = LoginPage(self.driver)
+        login.enter_username(username)
+        login.enter_username(password)
+        login.click_login()
+        error_msg = login.get_error_msg()
         assert_that(error_msg).is_equal_to(expected_title)
 
-
 class TestLoginUi(WebDriverWrapper):
-
     def test_title(self):
         actual_title = self.driver.title
         assert_that("OpenEMR Login").matches(actual_title)
