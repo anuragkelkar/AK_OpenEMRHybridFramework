09f09852e84896dcc357ccda614b3d19d018a45e
Kelkar <AKELKAR1@jaguarlandrover.com>
2024-02-07 06:11:38 +0000

    logger configuration

--- base/automation_wrapper.py
+++ base/automation_wrapper.py
@@ -1,15 +1,19 @@
+import logging
+
 import pytest
 from selenium import webdriver
 import pandas
 from utils import read_utils
 import config
 
+Logger = logging.getLogger()
 class WebDriverWrapper:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         #runs before each test method
         self.json_config = read_utils.get_json_data_into_dic(config.test_data_path+"\\config.json")
         browser = self.json_config.get("browser")
+        Logger.info(f"Launched browser {browser}")
         if browser == "edge":
             self.driver=webdriver.Edge()
         elif browser == "ff":
